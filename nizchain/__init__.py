d0762bbfbe376002ce0e26af6f7e14132f3dfb70
MuhammadNizamani <ishaque.niz786@gmail.com>
2024-06-01 16:42:42 +0500

    ENh: added confugration

---
+++ nizchain/__init__.py
@@ -0,0 +1,5 @@
+from .config import config
+
+configure = config.configure
+
+__all__ = ['configure']
---
+++ nizchain/config.py
@@ -0,0 +1,9 @@
+class Config:
+    def __init__(self) :
+        self.api_key = None
+        
+    def configure(self, api_key):
+        self.api_key = api_key
+        
+        
+config = Config()
